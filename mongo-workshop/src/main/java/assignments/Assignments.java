d235e1feb24febf92b26df00f8708cecaf8e527a
Sivar72 <sivarwerrie@hotmail.nl>
2017-01-13 13:01:59 +0100

    Assignments solution.

--- mongo-workshop/src/main/java/assignments/Assignments.java
+++ mongo-workshop/src/main/java/assignments/Assignments.java
@@ -1,11 +1,10 @@
 package assignments;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bson.Document;
-
-import com.mongodb.client.MongoCollection;
 
 import database.Database;
 
@@ -16,7 +15,7 @@ public class Assignments {
     private int gameDocs = 0;
 
     public Assignments() {
-        this.database = new Database();
+        this.database = Database.getInstance();
     }
 
     public void runAssignments() {
@@ -41,30 +40,58 @@ public class Assignments {
 
     private void runAssignment2() {
         System.out.println("Assignment2: ");
-        // TODO: Insert 3 users into the user collection with the following
-        // values:
-        // String name;
-        // List<Integer> games;
-        // Document address; with a country and postal code
-
-        // TODO: Insert 5 games into the game collection with the following
-        // values:
-        // int id;
-        // String name;
-        // double price;
         
         //Counting user docs
         database.getUserCollection().visit(() -> {
-            MongoCollection<Document> users = database.getDatabase().getCollection("users");
-            return users;
+            return database.getDatabase().getCollection("users");
         }, x -> x).find().iterator().forEachRemaining(doc -> userDocs++);
         //Counting game docs
-        database.getUserCollection().visit(() -> {
-            MongoCollection<Document> games = database.getDatabase().getCollection("games");
-            return games;
+        database.getGameCollection().visit(() -> {
+            return database.getDatabase().getCollection("games");
         }, x -> x).find().iterator().forEachRemaining(doc -> gameDocs++);
+        
+        if (userDocs < 3) {
+        	database.getUserCollection().visit(() -> {
+                return database.getDatabase().getCollection("users");
+            }, x -> x).insertMany(Arrays.asList(
+            		createUserdoc("Kees", Arrays.asList(4, 3, 10), createAddressDoc("Nederland", "4810BG")),
+            		createUserdoc("Sjaak", Arrays.asList(1, 3, 9), createAddressDoc("Nederland", "9184WD")),
+    				createUserdoc("Henk", Arrays.asList(1, 5, 7), createAddressDoc("Nederland", "4910ZP"))
+    			));
+        }
+        if (gameDocs < 5) {
+        	database.getGameCollection().visit(() -> {
+                return database.getDatabase().getCollection("games");
+            }, x -> x).insertMany(Arrays.asList(createGamedoc(1, "Mario", 10.50),
+            		createGamedoc(3, "Zelda", 12.50), createGamedoc(4, "COD", 40.00),
+            		createGamedoc(9, "Battlefield", 40.00), createGamedoc(7, "CSGO", 15.00)));
+        }
+        
         System.out.println(userDocs + " user docs found.");
         System.out.println(gameDocs + " game docs found.");
+    }
+    
+    private Document createGamedoc(int id, String name, double price) {
+    	Document gameDoc = new Document();
+    	gameDoc.put("id", id);
+    	gameDoc.put("name", name);
+    	gameDoc.put("price", price);
+    	return gameDoc;
+    }
+    
+    private Document createUserdoc(String name, List<Integer> games, Document address) {
+    	Document userDoc = new Document();
+    	userDoc.put("name", name);
+    	userDoc.put("games", games);
+    	userDoc.put("address", address);
+    	return userDoc;
+    }
+    
+    private Document createAddressDoc(String country, String postal) {
+    	Document address = new Document();
+    	address.put("country", country);
+    	address.put("postal", postal);
+    	return address;
     }
 
     private void runAssignment3() {
@@ -72,8 +99,7 @@ public class Assignments {
         // TODO: implement the UserDocument manager class
         
         UserDocumentManager userManager = new UserDocumentManager(database.getUserCollection().visit(() -> {
-            MongoCollection<Document> users = database.getDatabase().getCollection("users");
-            return users;
+            return database.getDatabase().getCollection("users");
         }, x -> x).find().first());
         System.out.println(userManager.getName());
         System.out.println(userManager.getGames());
@@ -85,8 +111,7 @@ public class Assignments {
         // TODO: implement the GameDocument manager class
         
         GameDocumentManager gameManager = new GameDocumentManager(database.getGameCollection().visit(() -> {
-            MongoCollection<Document> games = database.getDatabase().getCollection("games");
-            return games;
+            return database.getDatabase().getCollection("games");
         }, x -> x).find().first());
         System.out.println(gameManager.getId());
         System.out.println(gameManager.getName());
@@ -98,26 +123,30 @@ public class Assignments {
         // TODO: Get all game documents for a user
         
         UserDocumentManager userManager = new UserDocumentManager(database.getUserCollection().visit(() -> {
-            MongoCollection<Document> users = database.getDatabase().getCollection("users");
-            return users;
+            return database.getDatabase().getCollection("users");
         }, x -> x).find().first());
-//      List<Integer> games = userManager.getGames();
-        
+        List<Integer> games = userManager.getGames();
         List<Document> userGames = new ArrayList<Document>();
+        for (int game : games) {
+        	Document query = new Document();
+        	query.put("id", game);
+        	userGames.add(database.getGameCollection().visit(() -> {
+        		 return database.getDatabase().getCollection("games");
+        	}, x -> x).find(query).iterator().tryNext());
+        }
+        
         System.out.println(userGames.size() + " games found for " + userManager.getName());
     }
     
     private void runAssignment6() {
         System.out.println("Assignment6: ");
-        //TODO: change the name and address for a user, add these methods to the UserDocumentManager.
-        
-        
+        UserDocumentManager manager = new UserDocumentManager(database.getUserCol().find().first());
+        manager.setName("Bert");
+        manager.setAddress("USA", "1049GB");
     }
 
-    // TODO: implement methods
     private class GameDocumentManager {
 
-        @SuppressWarnings("unused")
         private final Document gameDoc;
 
         public GameDocumentManager(Document gameDoc) {
@@ -125,45 +154,69 @@ public class Assignments {
         }
 
         public int getId() {
-
-            return 0;
+        	return gameDoc.getInteger("id");
         }
 
         public String getName() {
-
-            return "None";
+        	return gameDoc.getString("name");
         }
 
         public double getPrice() {
-
-            return 0.0;
+        	return gameDoc.getDouble("price");
         }
 
     }
 
-    // TODO: implement methods
     private class UserDocumentManager {
 
-        @SuppressWarnings("unused")
-        private final Document userDoc;
+        private Document userDoc;
 
         public UserDocumentManager(Document userDoc) {
             this.userDoc = userDoc;
         }
 
         public String getName() {
-
-            return "no username found";
+        	return userDoc.getString("name");
         }
 
-        public List<Integer> getGames() {
-
-            return null;
+        @SuppressWarnings("unchecked")
+		public List<Integer> getGames() {
+        	return (List<Integer>) userDoc.get("games");
         }
 
         public Document getAddress() {
-
-            return null;
+        	return (Document) userDoc.get("address");
+        }
+        
+        public void setName(String name) {
+        	String oldName = getName();
+        	Document newValue = new Document();
+        	newValue.put("name", name);
+        	
+        	update(newValue);
+        	updateThisDoc(name);
+        	
+        	System.out.println(oldName + " changed to " + getName());
+        }
+        
+        public void setAddress(String country, String postal) {
+        	Document newAddress = new Document();
+        	newAddress.put("country", country);
+        	newAddress.put("postal", postal);
+        	Document newValue = new Document();
+        	newValue.put("address", newAddress);
+        	update(newValue);
+        }
+        
+        private void update(Document newValue) {
+        	Database.getInstance().getUserCol().findOneAndUpdate(userDoc, new Document("$set", newValue));
+        	updateThisDoc(getName());
+        }
+        
+        private void updateThisDoc(String nameValue) {
+        	Document newDoc = new Document();
+        	newDoc.put("name", nameValue);
+        	this.userDoc = Database.getInstance().getUserCol().find(newDoc).iterator().tryNext();
         }
 
     }
--- mongo-workshop/src/main/java/database/Database.java
+++ mongo-workshop/src/main/java/database/Database.java
@@ -8,16 +8,21 @@ import com.mongodb.client.MongoDatabase;
 
 import option.IOption;
 import option.None;
+import option.Some;
 
 public class Database {
     
     private final MongoClient mongo;
     private final MongoDatabase db;
+    private final MongoCollection<Document> gameCollection;
+    private final MongoCollection<Document> userCollection;
+    private static Database instance;
     
-    public Database() {
+	private Database() {
         this.mongo = new MongoClient("localhost", 27017);
         this.db = mongo.getDatabase("workshop");
-        //TODO: initialise collections
+        this.gameCollection = db.getCollection("games");
+        this.userCollection = db.getCollection("users");
     }
     
     public MongoClient getClient() {
@@ -30,12 +35,24 @@ public class Database {
     
     //TODO: return collection
     public IOption<MongoCollection<Document>> getGameCollection() {
-        return new None<MongoCollection<Document>>();
+    	if (gameCollection == null)
+    		 return new None<MongoCollection<Document>>();
+    	return new Some<MongoCollection<Document>>(gameCollection);
     }
     
     //TODO: return collection
     public IOption<MongoCollection<Document>> getUserCollection() {
-        return new None<MongoCollection<Document>>();
+    	if (userCollection == null)
+    		return new None<MongoCollection<Document>>();
+    	return new Some<MongoCollection<Document>>(userCollection);
+    }
+    
+    public MongoCollection<Document> getGameCol() {
+    	return gameCollection;
+    }
+    
+    public MongoCollection<Document> getUserCol() {
+    	return userCollection;
     }
     
     public void dropCollections() {
@@ -47,6 +64,12 @@ public class Database {
             x.drop();
             return "Collection destroyed.";
         });
+    }
+    
+    public static Database getInstance() {
+    	if (instance == null)
+    		instance = new Database();
+    	return instance;
     }
     
 }
